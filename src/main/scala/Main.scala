2bbf9dea34c4cca4b2d3996b2247ff6d445faa89
Robert Kleemann <robert@kleemann.org>
2025-07-14 18:07:00 -0700

    take either a file or diretory argument and put all output in the project out/ dir

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,35 @@
 
 //import org.apache.pdfbox.pdmodel.PDDocument
 
+// turn a single csv file into a pdf file
+def processFile(src: os.Path): Unit = {
+  val filename = src.baseName + "." + src.ext
+  Csv.parse(filename.toString, os.read.lines.stream(src)) match {
+    case Right(csv) => {
+      Pdf.create(csv) match {
+        case Right(doc) => {
+          val outfile = os.pwd / "out" / (src.baseName + ".pdf")
+          doc.save(outfile.toString)
+          doc.close()
+        }
+        case Left(error) => {
+          println(error)
+          sys.exit(1)
+        }
+      }
+    }
+    case Left(error) => {
+      println(error)
+      sys.exit(1)
+    }
+  }
+}
+
+def usage(): Unit = {
+  println("valmap ( <inputfile> | inputdir )")
+  sys.exit(1)
+}
+
 /** A non functional main with all the IO and side effects
   * 
   * @param args all command line args
@@ -12,31 +41,18 @@
     val outfile = os.pwd / "valmap-output.pdf"
     doc.save(outfile.toString)
     doc.close()
-  } else if (args.isEmpty) {
-    println("valmap <csvfile>")
-    sys.exit(1)
-  } else {
-    val file: os.Path = os.Path(args.head, os.pwd)
-    Csv.parse(file.toString, os.read.lines.stream(file)) match {
-      case Right(csv) => {
-        Pdf.create(csv) match {
-          case Right(doc) => {
-            val outfile = os.pwd / "valmap-output.pdf"
-            doc.save(outfile.toString)
-            doc.close()
-          }
-          case Left(error) => {
-            println(error)
-            sys.exit(1)
-          }
-        }
+  }
+  else if (args.isEmpty) usage()
+  else {
+    val fileOrDir: os.Path = os.Path(args.head, os.pwd)
+    if (os.isFile(fileOrDir)) {
+      processFile(fileOrDir)
+    } else if (os.isDir(fileOrDir)) {
+      os.list(fileOrDir).foreach { file =>
+        if (file.ext == "csv") processFile(file)
+        else println(s"WARNING: skiping file ${file.toString}")
       }
-      case Left(error) => {
-        println(error)
-        sys.exit(1)
-      }
-    }
-
+    } else usage()
     sys.exit(0)
   }
 }
